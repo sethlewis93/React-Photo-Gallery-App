6dd6daa0bec7bd071f585641f2e9c1d1acd1c7da
Seth <skiprjak93@protonmail.com>
2021-03-10 09:46:36 -0500

    establishes custom hooks document

---
+++ src/usePhotoState.js
@@ -0,0 +1,11 @@
+
+/**
+ * 
+ * itemize state shared throughout the app. decipher flow of data to and fro 
+ * create folder for hooks
+ * initialize state
+ * create any functions we need for modifying the state
+ * return whatever we want another component to have access to (count, increment)
+ * check out what variables are being displayed in the UI
+    
+ */
